860f92d2384a94bcc991b58b94e4f6d63ba01ed0
kadirdemirel <kadirdemirel_17@hotmail.com>
2024-07-08 11:36:05 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { CORE_CONCEPTS } from "./data";
+import CoreConcepts from "./components/CoreConcepts";
+import TabButton from "./components/TabButton";
+import Header from "./components/Header/Header";
+
+function App() {
+  function handleClick(selectedButton) {
+    console.log(selectedButton);
+  }
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleClick("Components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleClick("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleClick("State")}>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,4 @@
+export default function TabButton(props) {
+  
+    return <li><button onClick={props.onSelect}>{props.children}</button></li>;
+}
